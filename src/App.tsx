74abd261cf90b910eeb54adeb5ea05e1cb4306ac
Fidel Marques <fidel.marques@engenharia.ufjf.br>
2022-11-29 20:04:45 -0300

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { Router } from "./router/router";
+
+export const App = () => {
+  return (
+    <div className="App">
+      <Router />
+    </div>
+  );
+};
