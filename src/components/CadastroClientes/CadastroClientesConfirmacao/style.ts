4ac3aefc222d1f246a89b0171a3c0fca5573ce76
Fidel Marques <fidel.marques@engenharia.ufjf.br>
2022-12-01 16:22:50 -0300

    fix: alinhamento de DataContainer

--- src/components/CadastroClientes/CadastroClientesConfirmacao/style.ts
+++ src/components/CadastroClientes/CadastroClientesConfirmacao/style.ts
@@ -3,14 +3,16 @@ import styled from "styled-components";
 export const DataContainer = styled.div`
   display: flex;
   justify-content: space-between;
-  width: 100%;
+  width: 50%;
+  max-width: 400px;
   font-size: 20px;
+  align-items: center;
 `;
 
 export const InfoContainer = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
   gap: 5px;
-  width: 50%;
   font-size: 16px;
 `;
